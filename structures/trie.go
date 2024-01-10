188cbf4346444dc7fb794723d8f06889af5f01cc
Hickar <hickar@icloud.com>
2024-01-10 16:06:53 +0300

    Suggestion algorithm for trie

--- structures/trie.go
+++ structures/trie.go
@@ -1,5 +1,9 @@
 package structures
 
+import (
+	"sort"
+)
+
 type TrieNode struct {
 	children map[rune]*TrieNode
 	isWord   bool
@@ -19,41 +23,92 @@ func (t *TrieNode) InsertAll(words []string) {
 
 func (t *TrieNode) Insert(word string) {
 	var (
-		curNode  = t
-		nextNode *TrieNode
-		ok       bool
+		cur  = t
+		next *TrieNode
+		ok   bool
 	)
 
 	for _, c := range word {
-		nextNode, ok = curNode.children[c]
+		next, ok = cur.children[c]
 		if !ok {
-			nextNode = NewTrie()
-			curNode.children[c] = nextNode
+			next = NewTrie()
+			cur.children[c] = next
 		}
 
-		curNode = nextNode
+		cur = next
 	}
 
-	curNode.isWord = true
+	cur.isWord = true
 }
 
-func (t *TrieNode) Search(word string) bool {
+func (t *TrieNode) Contains(word string) bool {
 	var (
-		curNode  = t
-		nextNode *TrieNode
-		ok       bool
+		cur  = t
+		next *TrieNode
+		ok   bool
 	)
 
 	for _, c := range word {
-		nextNode, ok = curNode.children[c]
+		next, ok = cur.children[c]
 		if !ok {
 			return false
 		}
 
-		curNode = nextNode
+		cur = next
 	}
 
-	return curNode.isWord
+	return cur.isWord
+}
+
+func (t *TrieNode) Search(searchWord string, limit int) []string {
+	if limit < 1 {
+		return nil
+	}
+
+	var (
+		matches []string
+		cur     = t
+		next    *TrieNode
+		ok      bool
+	)
+
+	for _, c := range searchWord {
+		next, ok = cur.children[c]
+		if !ok {
+			return matches
+		}
+
+		cur = next
+	}
+
+	if cur.isWord {
+		matches = append(matches, searchWord)
+	}
+	for c, child := range cur.children {
+		matches = append(matches, child.getSuggestions(searchWord+string(c))...)
+	}
+
+	sort.Strings(matches)
+
+	if len(matches) <= limit {
+		return matches
+	}
+
+	return matches[:limit]
+}
+
+func (t *TrieNode) getSuggestions(prefix string) []string {
+	var suggestions []string
+
+	for c, child := range t.children {
+		if child.isWord {
+			suggestions = append(suggestions, prefix+string(c))
+		}
+
+		suggestions = append(suggestions, child.getSuggestions(prefix+string(c))...)
+	}
+
+	return suggestions
 }
 
 func (t *TrieNode) Remove(word string) bool {
@@ -62,40 +117,59 @@ func (t *TrieNode) Remove(word string) bool {
 	}
 
 	var (
-		curNode        = t
-		nextNode       *TrieNode
+		cur            = t
+		next           *TrieNode
 		ok             bool
 		deleteToken    = rune(word[0])
 		depthDeleteIdx int
 	)
 
 	for i, c := range word {
-		if (curNode.isWord && !curNode.isLeaf()) || len(curNode.children) > 1 {
+		if (cur.isWord && !cur.isLeaf()) || len(cur.children) > 1 {
 			depthDeleteIdx = i
 			deleteToken = c
 		}
 
-		nextNode, ok = curNode.children[c]
+		next, ok = cur.children[c]
 		if !ok {
 			return false
 		}
 
-		curNode = nextNode
+		cur = next
 	}
 
-	if !curNode.isLeaf() || !curNode.isWord {
+	if !cur.isLeaf() || !cur.isWord {
 		return false
 	}
 
-	curNode = t
+	cur = t
 	for i := 0; i < depthDeleteIdx; i++ {
-		curNode = curNode.children[rune(word[i])]
+		cur = cur.children[rune(word[i])]
 	}
 
-	delete(curNode.children, deleteToken)
+	delete(cur.children, deleteToken)
 	return true
 }
 
 func (t *TrieNode) isLeaf() bool {
 	return len(t.children) == 0
+}
+
+func (t *TrieNode) getNode(searchWord string) *TrieNode {
+	var (
+		cur  = t
+		next *TrieNode
+		ok   bool
+	)
+
+	for _, c := range searchWord {
+		next, ok = cur.children[c]
+		if !ok {
+			return nil
+		}
+
+		cur = next
+	}
+
+	return cur
 }
--- structures/trie_test.go
+++ structures/trie_test.go
@@ -22,14 +22,35 @@ func TestTrieInsert(t *testing.T) {
 	assert.Len(t, trie.children['t'].children['r'].children, 2)
 }
 
+func TestTrieIsWord(t *testing.T) {
+	trie := NewTrie()
+	trie.InsertAll([]string{"mobile", "mouse", "moneypot", "monitor", "mousepad"})
+
+	assert.NotPanics(t, func() {
+		assert.True(t, trie.getNode("mouse").isWord)
+		assert.True(t, trie.getNode("mousepad").isWord)
+	})
+}
+
 func TestTrieSearch(t *testing.T) {
+	input := []string{"mobile", "mouse", "moneypot", "monitor", "mousepad"}
+	trie := NewTrie()
+	trie.InsertAll(input)
+
+	expected := []string{"mouse", "mousepad"}
+	output := trie.Search("mouse", 3)
+
+	assert.Equal(t, expected, output)
+}
+
+func TestTrieContains(t *testing.T) {
 	trie := NewTrie()
 	trie.Insert("trie")
 	trie.Insert("tree")
 
-	assert.True(t, trie.Search("trie"))
-	assert.True(t, trie.Search("tree"))
-	assert.False(t, trie.Search("tr"))
+	assert.True(t, trie.Contains("trie"))
+	assert.True(t, trie.Contains("tree"))
+	assert.False(t, trie.Contains("tr"))
 }
 
 func TestTrieRemove(t *testing.T) {
@@ -40,11 +61,11 @@ func TestTrieRemove(t *testing.T) {
 	assert.False(t, trie.Remove("tr"))
 
 	assert.True(t, trie.Remove("tree"))
-	assert.False(t, trie.Search("tree"))
+	assert.False(t, trie.Contains("tree"))
 
-	assert.True(t, trie.Search("trie"))
+	assert.True(t, trie.Contains("trie"))
 	assert.True(t, trie.Remove("trie"))
-	assert.False(t, trie.Search("trie"))
+	assert.False(t, trie.Contains("trie"))
 
 	assert.Len(t, trie.children, 0)
 }
